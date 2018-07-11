7b4f8869596bdf211f76e56d1ce01af220664015
allanmusic <40580479+allanmusic@users.noreply.github.com>
2018-07-11 08:37:17 -0600

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -38,12 +38,9 @@ $("input[type=text]").on("keypress",function(e){
 
   let password = $(this).val();
 
- if(e.which===13){
-    console.log($(this).val());
-    
-    let array = ["allan","nelson","adolfo","nela","miguel","josue","nadia","paz","valeria"];
-
-    if( array.indexOf(password) === -1){
+  if(e.which===13){
+   
+    if( password !== "CRSRBU"){
       $("#purchaseWrong").show();
       $("#purchase").hide();
       $(this).val("");
@@ -54,5 +51,4 @@ $("input[type=text]").on("keypress",function(e){
       $(this).val("");
     }
   }
-  
 });
